1dc5f4c848dc834575d11eb9a63596d6d0507640
RazelRaz <razel16dcm@gmail.com>
2023-08-21 22:14:27 +0600

    blog api data added in blog and single blog

---
+++ app/blog/[id]/page.js
@@ -0,0 +1,19 @@
+import getBlogPost from '@/lib/getBlogPost';
+import React from 'react';
+import styles from './singleblog.module.css'
+
+const SinglePage = async ({params}) => {
+    const id = params.id;
+    const post = await getBlogPost(id);
+    // console.log(post);
+
+    return (
+        <div className={styles.singleBLog}>
+            <figure><img src={post?.['postDetails']['img']} alt="Blog image" /></figure>
+            <h2>{post['postDetails']['title']}</h2>
+            <p>{post['postDetails']['content']}</p>
+        </div>
+    );
+};
+
+export default SinglePage;
--- app/blog/page.js
+++ app/blog/page.js
@@ -1,13 +1,34 @@
 import React from 'react';
 import styles from './blog.module.css'
+import getAllPosts from '@/lib/getAllPosts';
+import Link from 'next/link';
 
-const BlogPage = () => {
+const BlogPage = async () => {
+
+    const posts = await getAllPosts()
+    // console.log(posts);
+
     return (
         <div className={styles.blogArea}>
             <div className={styles.aboutBreadCrumb}>
                 <h1>BLOG</h1>
             </div>
-            
+            <div className={styles.blogAll}>
+                {
+                    posts.map(post => {
+                        return (
+                            <div key={post.id}>
+                                <img src={post.img} alt='image' />
+                                <div className={styles.innerBlog}>
+                                    <h2>{post.title}</h2>
+                                    <p>{post.short}</p>
+                                    <Link href={`/blog/${post.id}`}>Read More</Link>
+                                </div>
+                            </div>
+                        )
+                    })
+                }
+            </div>
         </div>
     );
 };

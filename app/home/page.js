13551e18cb878f934ef631a1d625ce5a962bd9a7
RazelRaz <razel16dcm@gmail.com>
2023-08-21 13:02:40 +0600

    hero area done

--- app/home/page.js
+++ app/home/page.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import styles from './home.module.css'
+import Link from 'next/link';
 
 const HomePage = () => {
     return (
-        <div>
-            <h1>Home Page</h1>
+        <div className={styles.homeArea}>
+            <div className={styles.homebox}>
+                <div className={styles.homeLeft}>
+                    <h1>Welcome To The Next Blog</h1>
+                    <p>We do this with the aim of creating the future of media. The future of media is one that is disruptive, agile, and credible.</p>
+                    <Link href={'/blog'}>Know More</Link>
+                </div>
+            </div>
         </div>
     );
 };

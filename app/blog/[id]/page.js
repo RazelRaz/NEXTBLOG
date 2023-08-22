18524e6055a4f7b738f0a426a793f9c4447c2391
RazelRaz <razel16dcm@gmail.com>
2023-08-22 11:47:59 +0600

    optional chaining added

--- app/blog/[id]/page.js
+++ app/blog/[id]/page.js
@@ -9,10 +9,10 @@ const SinglePage = async ({params}) => {
 
     return (
         <div className={styles.singleBLog}>
-            <figure><img src={post?.['postDetails']['img']} alt="Blog image" /></figure>
-            <h2>{post['postDetails']['title']}</h2>
-            <small>{post['postDetails']['created_at']}</small>
-            <p>{post['postDetails']['content']}</p>
+            <figure><img src={post?.['postDetails']?.['img']} alt="Blog image" /></figure>
+            <h2>{post?.['postDetails']?.['title']}</h2>
+            <small>{post?.['postDetails']?.['created_at']}</small>
+            <p>{post?.['postDetails']?.['content']}</p>
         </div>
     );
 };
